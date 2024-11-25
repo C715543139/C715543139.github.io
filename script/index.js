3b86ddfccdcc568df68640bccd65e9585f669afc
C715543139 <231880058@smail.nju.edu.cn>
2024-11-25 11:20:27 +0800

    v24.11.25 Add 2 guys. Resize multiple elements. Make it scrolling.

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 function TitleChange(){
-    document.title = ["Page From C715543139", "每日commit挑战中"][Number(document.visibilityState === "visible")];
+    document.title = ["Page From C715543139", "每日 commit 挑战中"][Number(document.visibilityState === "visible")];
 }
 
 TitleChange();
